d8e49317ceb093ad417941766fc30bc4690fd758
Johnny Faria <johnnys.faria@gmail.com>
2022-04-13 22:21:21 -0300

    exclusão de itens na tabela

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,23 +14,37 @@ var people = [
     tel: '14999999999',
     xp: false,
   },
+  {
+    name: 'Adielen Silva de Faria',
+    tel: '14999999999',
+    xp: false,
+  },
 ];
 
-for (person in people) {
-  document.querySelector(
-    'table.lista tbody',
-  ).innerHTML += `<tr style="background-color: ${
-    person % 2 == 0 ? '#FFF' : '#EEE'
-  }" >
+function desenhaTabela() {
+  currentLines = [
+    ...document.querySelectorAll('table.lista tbody .dinamic-content'),
+  ];
+  currentLines.forEach((element) => {
+    element.remove();
+  });
+  for (person in people) {
+    document.querySelector(
+      'table.lista tbody',
+    ).innerHTML += `<tr class="dinamic-content" style="background-color: ${
+      person % 2 == 0 ? '#FFF' : '#EEE'
+    }" >
   <td>${people[person].name}</td>
   <td>${people[person].tel}</td>
   <td>${
     people[person].xp
-      ? '<span style="color:green"> Sim </span>'
-      : '<span style="color:red"> Não </span>'
+      ? '<strong style="color:green"> Sim </strong>'
+      : '<strong style="color:red"> Não </strong>'
   }</td>
   <td>
-      <button>Alterar</button>
+      <button onClick="people.splice(${person}, 1); desenhaTabela()">Excluir</button>
     </td>
   </tr>`;
+  }
 }
+desenhaTabela();
