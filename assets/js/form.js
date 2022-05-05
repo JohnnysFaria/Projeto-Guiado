5bc1a925a47ac386a057f02ae4537aba1e629a39
Johnny Faria <johnnys.faria@gmail.com>
2022-05-05 02:11:04 -0300

    regex telefone

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,24 @@
 function testaFormulario(e) {
   e.preventDefault();
 
+  // for (i in e.target.elements['phone'].value) {
+  //   if ('0123456789'.indexOf(e.target.elements['phone'].value[i]) == -1) {
+  //     alert('Apenas números são permitidos nesse campo');
+  //     return false;
+  //   }
+  // }
+
+  var phonePattern = /[^0-9-() ]+/g;
+  if (phonePattern.test(e.target.elements['phone'].value)) {
+    alert('Apenas números são permitidos nesse campo');
+    return false;
+  }
+
+  if (e.target.elements['phone'].value.replace(/[-() ]/g, '').length < 11) {
+    alert('Número invalido!');
+    return false;
+  }
+
   var peopleRaw = localStorage.getItem('people');
   if (peopleRaw != null) {
     var people = JSON.parse(peopleRaw);
@@ -44,5 +62,24 @@ if (id !== null) {
     document.getElementById('xp-yes').checked = true;
   } else {
     document.getElementById('xp-yes').checked = true;
+  }
+}
+
+function testaCampoTelefone(e) {
+  e.preventDefault();
+  console.log(e);
+
+  if (e.target.value.length == 0) {
+    e.target.value += '(';
+  }
+  if (e.target.value.length == 3) {
+    e.target.value += ') ';
+  }
+  if (e.target.value.length == 10) {
+    e.target.value += '-';
+  }
+
+  if (/[0-9]/g.test(e.key) && e.target.value.length < 15) {
+    e.target.value += e.key;
   }
 }
